b7d568eb60576d8bd0e7e8012c111800ba0afbdc
mlhale <mlhale@unomaha.edu>
2018-12-24 02:46:05 -0600

    working on count aggregator

--- api/views.py
+++ api/views.py
@@ -3,7 +3,7 @@
 # @Email:  mlhale@unomaha.edu
 # @Filename: views.py
 # @Last modified by:   matthale
-# @Last modified time: 2018-12-24T02:42:44-06:00
+# @Last modified time: 2018-12-24T02:45:57-06:00
 # @License: Funset is a web-based BIOI tool for visualizing genetic pathway information. This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
 # @Copyright: Copyright (C) 2017 Matthew L. Hale, Dario Ghersi, Ishwor Thapa
 
@@ -119,7 +119,7 @@ class TermViewSet(viewsets.ModelViewSet):
 		
 		if namespace is not None and ontology_id is not None:
 			if namespace is 'bp':
-				name_query = "[u'biological_process]"
+				name_query = "[u'biological_process']"
 				
 			objs = Term.objects.filter(ontology__id=ontology_id, namespace=namequery).order_by('termid').count()
 			return Response({'count': objs, 'pages': int(math.ceil(float(objs)/settings.REST_FRAMEWORK['PAGE_SIZE']))})
