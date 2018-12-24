4a992b66b8387a79c57675dcbdef9277cb7730a2
mlhale <mlhale@unomaha.edu>
2018-12-24 02:19:14 -0600

    updating created date for ontology

--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@
 # @Email:  mlhale@unomaha.edu
 # @Filename: models.py
 # @Last modified by:   matthale
-# @Last modified time: 2018-12-23T04:23:33-06:00
+# @Last modified time: 2018-12-24T02:15:03-06:00
 # @License: Funset is a web-based BIOI tool for visualizing genetic pathway information. This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
 # @Copyright: Copyright (C) 2017 Matthew L. Hale, Dario Ghersi, Ishwor Thapa
 
@@ -27,7 +27,7 @@ class EagerLoadingMixin:
         return queryset
 
 class Ontology(models.Model):
-    created = models.DateField(auto_now_add=True)
+    created = models.DateField()
     name = models.CharField(max_length=1000, blank=False, unique=True)
     description = models.CharField(max_length=1000, blank=True)
 
