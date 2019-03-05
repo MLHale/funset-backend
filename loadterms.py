7462275f582fc65ca961e9f2e1693d1c4302f5bb
Matt Hale <mlhale@unomaha.edu>
2019-03-05 12:58:31 -0600

    multithreaded the loadterm script

--- loadterms.py
+++ loadterms.py
@@ -3,7 +3,7 @@
 # @Email:  mlhale@unomaha.edu
 # @Filename: loadterms.py
 # @Last modified by:   mlhale
-# @Last modified time: 2019-03-05T12:52:55-06:00
+# @Last modified time: 2019-03-05T12:58:24-06:00
 # @License: Funset is a web-based BIOI tool for visualizing genetic pathway information. This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
 # @Copyright: Copyright (C) 2017 Matthew L. Hale, Dario Ghersi, Ishwor Thapa
 
@@ -181,7 +181,7 @@ if __name__ == '__main__':
         manager = Manager()
         lock = manager.Lock()
         taskworker = partial(loadTermParentsWorker, lock)
-        pool = Pool(8)
+        pool = Pool(5)
         tokens = [(key, term, ontology) for key, term in oboDict.items()]
 
         pool.map(taskworker, tokens)
