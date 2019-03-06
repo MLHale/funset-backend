5b2f46b2ce1069e2f863a41ba8d8f23c6d9c0034
Matt Hale <mlhale@unomaha.edu>
2019-03-06 00:58:50 -0600

    updated debugging info

--- pathway_viz_backend/localsettings.py
+++ pathway_viz_backend/localsettings.py
@@ -3,7 +3,7 @@
 # @Email:  mlhale@unomaha.edu
 # @Filename: localsettings.py
 # @Last modified by:   matthale
-# @Last modified time: 2019-03-05T23:24:53-06:00
+# @Last modified time: 2019-03-06T00:58:25-06:00
 # @License: Funset is a web-based BIOI tool for visualizing genetic pathway information. This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
 # @Copyright: Copyright (C) 2017 Matthew L. Hale, Dario Ghersi, Ishwor Thapa
 
@@ -31,4 +31,4 @@ DATABASES = {
 ALLOWED_HOSTS = ['137.48.191.135','localhost', 'django']
 
 # Set to DEV for debug and other configuration items.  PROD otherwise...
-ENVIRONMENT = "DEV"
+ENVIRONMENT = "PROD"
